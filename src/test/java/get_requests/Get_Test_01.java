f1da1187de7686b06b80f361ad0f26619469654d
tatarogluemre <tatarogluemre25@gmail.com>
2023-03-11 23:52:16 +0300

    11.03.23

---
+++ src/test/java/get_requests/Get_Test_01.java
@@ -0,0 +1,40 @@
+package get_requests;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.get;
+
+public class Get_Test_01 {
+
+//        Given
+//            https://restful-booker.herokuapp.com/booking/55
+//        When
+//            User sends a GET Request to the url
+//        Then
+//            HTTP Status Code should be 200
+//        And
+//            Content Type should be JSON
+//        And
+//            Status Line should be HTTP/1.1 200 OK
+//
+
+
+    @Test
+    public void get01() {
+//        -- Set the URL
+        String url = " https://restful-booker.herokuapp.com/booking/55 ";
+
+//        -- Set the expected data
+
+//        -- Send the request and get the reponse
+        Response response = get(url);
+        response.prettyPrint();
+//        -- Do assertion
+        response.
+                then().             //then metodu beklenen değeri assertion yapar.
+                statusCode(200).
+                contentType("application/json").
+                statusLine("HTTP/1.1 200 OK");
+    }
+}
---
+++ src/test/java/get_requests/RequestResponse.java
@@ -0,0 +1,49 @@
+package get_requests;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.post;
+
+public class RequestResponse {
+    /*
+    1-) Postman manuel API testi için kullanılır
+    2-) API otomosyonu için Rest-Assured Library kullanıcaz
+    3-) Otomasyon kodlarının yazımı için şu adımları izliyoruz :
+         a- Gereksinimleri anlama
+         b- Test Case'i yazma :
+            - Test Case yazmak için "Gherkin Language" kullanıyoruz.
+               -- Given:  Ön Koşullar --> EndPoint, body
+               -- When :  İşlemler --> Get, Put, Delete ...
+               -- Then :  Dönütler--> Assert
+               -- And  :  Çoklu işlemlerin art arda yazılacağı yerlerde kullanılır.
+          c- Test kodunu yazarken şu adımları izleriz :
+               -- Set the URL
+               -- Set the expected data
+               -- Send the request and get the reponse
+               -- Do assertion
+     */
+
+    public static void main(String[] args) {
+        // Get request nasıl yapılır :
+        Response response = get("https://restful-booker.herokuapp.com/booking/198");
+        response.prettyPrint();//prettyPrint() methodu reponse datayı Json formatında yazdırır.
+       //Status Code Yazdırma:
+        System.out.println("Status Code :"+response.statusCode());
+       //Contetnt-type yazdırma:
+        System.out.println("Content Type : "+response.contentType());
+      //Status Line yazdırma:
+        System.out.println("Status Line :"+response.statusLine());
+      //Header yazdırma:
+        System.out.println("Connection :"+response.header("Connection"));
+        System.out.println("Server :"+response.header("Server"));
+      //Headers yazdırma:
+        System.out.println("Headers : \n"+response.headers());
+      //Time yazdırma:
+        System.out.println("Time : "+response.getTime());
+
+
+    }
+
+
+}
